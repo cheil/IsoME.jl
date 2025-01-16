3c3a88b9f678672bd1bb8d56868c8460895eb570
Dominik Spath <dominik.spath@student.tugraz.at>
2025-01-16 15:41:16 +0100

    Error handling

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,11 @@ using Test
                     a2f_file = joinpath(@__DIR__, "Nb/Nb.a2F"),
                     dos_file = joinpath(@__DIR__, "Nb/Nb.dos"),
                     Weep_file = joinpath(@__DIR__, "Nb/Weep.dat"),
+                    Wen_file = joinpath(@__DIR__, "Nb/Wen.dat"),
                     flag_figure = 0,
                     returnTc    = true,
                     outdir = outdir*"cDOS_mu/",
+                    muc_AD = 0.12,
                     )
 
     Tc = EliashbergSolver(inp)
@@ -24,14 +26,15 @@ using Test
 
     ### 2.TEST: Nb vDOS mu* ###
     inp = arguments(
-                    a2f_file = "test/Nb/Nb.a2F",
-                    dos_file = "test/Nb/Nb.dos",
-                    Weep_file = "test/Nb/Weep.dat",
-                    Wen_file = "test/Nb/Wen.dat",
+                    a2f_file = joinpath(@__DIR__, "Nb/Nb.a2F"),
+                    dos_file = joinpath(@__DIR__, "Nb/Nb.dos"),
+                    Weep_file = joinpath(@__DIR__, "Nb/Weep.dat"),
+                    Wen_file = joinpath(@__DIR__, "Nb/Wen.dat"),
                     flag_figure = 0,
                     returnTc    = true,
                     outdir = outdir*"vDOS_mu/",
                     cDOS_flag   = 0,
+                    muc_AD = 0.12,
                     )
 
     Tc = EliashbergSolver(inp)
@@ -40,10 +43,10 @@ using Test
 
     ### 3.TEST: Nb cDOS W ###
     inp = arguments(
-        a2f_file="test/Nb/Nb.a2F",
-        dos_file="test/Nb/Nb.dos",
-        Weep_file="test/Nb/Weep.dat",
-        Wen_file="test/Nb/Wen.dat",
+        a2f_file = joinpath(@__DIR__, "Nb/Nb.a2F"),
+        dos_file = joinpath(@__DIR__, "Nb/Nb.dos"),
+        Weep_file = joinpath(@__DIR__, "Nb/Weep.dat"),
+        Wen_file = joinpath(@__DIR__, "Nb/Wen.dat"),
         flag_figure=0,
         returnTc=true,
         outdir=outdir * "cDOS_W/",
@@ -57,10 +60,10 @@ using Test
 
     ### 4.TEST: Nb vDOS W ###
     inp = arguments(
-        a2f_file="test/Nb/Nb.a2F",
-        dos_file="test/Nb/Nb.dos",
-        Weep_file="test/Nb/Weep.dat",
-        Wen_file="test/Nb/Wen.dat",
+        a2f_file = joinpath(@__DIR__, "Nb/Nb.a2F"),
+        dos_file = joinpath(@__DIR__, "Nb/Nb.dos"),
+        Weep_file = joinpath(@__DIR__, "Nb/Weep.dat"),
+        Wen_file = joinpath(@__DIR__, "Nb/Wen.dat"),
         flag_figure=0,
         returnTc=true,
         outdir=outdir * "vDOS_W/",

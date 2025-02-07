6b790b06ac33c4cb73419dc8b4c4df1e458730f4
Dominik Spath <dominik.spath@student.tugraz.at>
2025-02-07 16:45:04 +0100

    docs again

--- docs/make.jl
+++ docs/make.jl
@@ -31,7 +31,7 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/cheil/IsoME.jl",
+    repo = "github.com/cheil/IsoME.jl.git",
     push_preview = true,
     versions = nothing,
     branch = "gh-pages",
